2af330331e1e440f5368dc4e45842555065cd04c
ZZZZAO <simaocarmolopes@gmail.com>
2022-09-20 07:44:31 +0100

    Lab 3.03 Solved

--- Lab SQL Subqueries 3.03.sql
+++ Lab SQL Subqueries 3.03.sql
@@ -1,24 +1,24 @@
 -- 1.How many copies of the film Hunchback Impossible exist in the inventory system?
 
-SELECT COUNT(film_id)
-FROM inventory
-WHERE film_id = (SELECT film_id
-FROM film
-WHERE title = 'Hunchback Impossible');
+select count(film_id)
+from inventory
+where film_id = (select film_id
+from film
+where title = 'Hunchback Impossible');
 
 -- 2.List all films whose length is longer than the average of all the films.
 
-SELECT title
-FROM film
-WHERE length > (SELECT avg(length)
-FROM film);
+select title
+from film
+where length > (select avg(length)
+from film);
     
 -- 3.Use subqueries to display all actors who appear in the film Alone Trip.
     
-SELECT concat(first_name, ' ' , last_name) as Actor
-FROM actor
-WHERE actor_id IN (SELECT actor_id
-FROM film
+select concat(first_name, ' ' , last_name) as Actor
+from actor
+where actor_id in (select actor_id
+from film
 join film_actor using (film_id)
 where title = 'Alone Trip');
 
@@ -27,10 +27,10 @@ where title = 'Alone Trip');
 select title
 from film
 join film_category using(film_id)
-where category_id IN(
-SELECT category_id
-FROM sakila.category
-WHERE name = 'Family');
+where category_id in(
+select category_id
+from sakila.category
+where name = 'Family');
     
 -- 5.Get name and email from customers from Canada using subqueries. Do the same with joins. Note that to create a join, you will have to identify the correct tables with their primary keys and foreign keys, that will help you get the relevant information.
 
@@ -59,13 +59,13 @@ select title as Movies
 from film
 join film_actor using (film_id) where
 actor_id =(
-SELECT actor_id
-FROM sakila.actor
-INNER JOIN sakila.film_actor USING (actor_id)
-INNER JOIN sakila.film USING (film_id)
-GROUP BY actor_id
-ORDER BY COUNT(film_id) DESC 
-LIMIT 1);
+select actor_id
+from sakila.actor
+inner join sakila.film_actor using (actor_id)
+inner join sakila.film using (film_id)
+group by actor_id
+order by count(film_id) desc 
+limit 1);
 
 
 -- 7.Films rented by most profitable customer. You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments
@@ -77,22 +77,22 @@ select film_id
 from rental
 join inventory using (inventory_id)
 where customer_id =(
-SELECT customer_id
-FROM customer
-INNER JOIN payment USING (customer_id)
-GROUP BY customer_id
-ORDER BY sum(amount) DESC
-Limit 1));
+select customer_id
+from customer
+inner join payment using (customer_id)
+group by customer_id
+order by sum(amount) desc
+limit 1));
 
 -- 8.Customers who spent more than the average payments.
 
-SELECT CONCAT(first_name, ' ', last_name) as 'Customer above average spending'
-FROM customer
-JOIN payment USING (customer_id)
-GROUP BY customer_id
-HAVING SUM(amount) > (
-SELECT AVG(t1.total)
-FROM
-(SELECT SUM(amount) as total
-FROM payment
-GROUP BY customer_id) t1);
+select concat(first_name, ' ', last_name) as 'Customer above average spending'
+from customer
+join payment using (customer_id)
+group by customer_id
+having sum(amount) > (
+select avg(t1.total)
+from
+(select sum(amount) as total
+from payment
+group by customer_id) t1);
